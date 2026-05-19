7bbf859c05d09686778d960c7ecf8494b8a82bce
chris <Christopher.Mensah@Student.HTW-Berlin.de>
2026-05-19 22:10:21 +0200

    Add Docker setup

--- src/main/java/de/htw_berlin/FitnessTrainer/FitnessTrainerController.java
+++ src/main/java/de/htw_berlin/FitnessTrainer/FitnessTrainerController.java
@@ -6,7 +6,10 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5174")
+@CrossOrigin(origins = {
+"http://localhost:5173",
+"http://localhost:5174"
+})
 public class FitnessTrainerController {
 
     @GetMapping("/fitnesstrainer")
