f97c20599911a7ecdd7e4f6564c2de902aa272a4
chris <Christopher.Mensah@Student.HTW-Berlin.de>
2026-05-19 22:36:05 +0200

    cors fix

--- src/main/java/de/htw_berlin/FitnessTrainer/FitnessTrainerController.java
+++ src/main/java/de/htw_berlin/FitnessTrainer/FitnessTrainerController.java
@@ -6,10 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = {
-"http://localhost:5173",
-"http://localhost:5174"
-})
+@CrossOrigin(origins = "*")
 public class FitnessTrainerController {
 
     @GetMapping("/fitnesstrainer")
